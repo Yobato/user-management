1f48b57d4b1b8227c87b4bb513ff684b110d4288
Yobato <satriyobagus30077@gmail.com>
2025-09-17 17:23:17 +0700

    feat: create produk; fix: sidebar link and breadcrum

--- src/app/pages/produk/produk-routing-module.ts
+++ src/app/pages/produk/produk-routing-module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProdukPage } from './produk.component';
+import { CreateProdukPage } from './create/create.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ProdukPage,
+  },
+  {
+    path: 'create',
+    component: CreateProdukPage,
   }
 ];
 
